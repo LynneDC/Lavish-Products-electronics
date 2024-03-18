f730db3d78a3b9f42fe3d7a4e95f9065df1550e3
koechMR <briankipkoech328@gmail.com>
2024-03-18 17:43:56 +0300

    react application

---
+++ lavish-frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './Componets/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
