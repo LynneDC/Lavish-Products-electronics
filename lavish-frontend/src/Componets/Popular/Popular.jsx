21eb1457f1ca180fcf46b70f0d5055cfb932a8dd
koechMR <briankipkoech328@gmail.com>
2024-03-19 17:39:37 +0300

    new

--- lavish-frontend/src/Componets/Popular/Popular.jsx
+++ lavish-frontend/src/Componets/Popular/Popular.jsx
@@ -8,22 +8,7 @@ export const Popular = () => {
         <h1>POPULAR IN WOMEN</h1>
         <hr />
         <div className='popular-item'>
-            {data_products.map((product) => {
-                return (
-                    <div className='item'>
-                        <img src={product.image} alt={product.title} />
-                        <p>{product.name}</p>
-                        <div className='item-prices'>
-                            <div className='item-price-new'>
-                                {product.new_price}
-                            </div>
-                            <div className='item-price-old'>
-                                {product.old_price}
-                            </div>
-                        </div> 
-                    </div>
-                )
-            })}
+            {data_products}
 
     </div>
   )
