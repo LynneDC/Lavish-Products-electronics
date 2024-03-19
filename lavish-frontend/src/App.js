5f6e51a55672520951f3f185e0db27a59a6d2145
BcodezMUJJE <125450966+BcodezMUJJE@users.noreply.github.com>
2024-03-19 05:52:12 +0000

    updated

--- lavish-frontend/src/App.js
+++ lavish-frontend/src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
 import Navbar from './Componets/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
-      
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/shop' element={<ShopCategory category="Shop"/>}/>
+        <Route path='/cart' element={<ShopCategory category="Cart"/>}/>
+        <Route path='/product' element={<Product/>}/>
+          <Routes path='productId' element={<Product/>}/>
+      </Routes>
+      <Route path='/cart' element={<Cart/>}/>
+      </BrowserRouter>
     </div>
   );
 }
